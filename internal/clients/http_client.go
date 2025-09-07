a1ebb3276877263b7086a40acf6e2744c2a8c1ef
rpanchyk <ruslan.panchik@gmail.com>
2025-09-07 12:09:45 +0300

    Update http client to send headers

--- internal/clients/http_client.go
+++ internal/clients/http_client.go
@@ -1,5 +1,6 @@
 package clients
 
 type HttpClient interface {
-	Get(url string) (string, error)
+	Get(url string) (int, string, error)
+	GetWithHeaders(url string, headers map[string]string) (int, string, error)
 }
--- internal/clients/simple_http_client.go
+++ internal/clients/simple_http_client.go
@@ -3,25 +3,41 @@ package clients
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
 type SimpleHttpClient struct {
 }
 
-func (c SimpleHttpClient) Get(url string) (string, error) {
-	response, err := http.Get(url)
+func (c SimpleHttpClient) Get(url string) (int, string, error) {
+	return c.GetWithHeaders(url, nil)
+}
+
+func (c SimpleHttpClient) GetWithHeaders(url string, headers map[string]string) (int, string, error) {
+	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return "", fmt.Errorf("error sending http request: %w", err)
+		log.Fatalf("error creating request: %v", err)
+	}
+
+	if headers != nil {
+		for name, value := range headers {
+			request.Header.Add(name, value)
+		}
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		return 0, "", fmt.Errorf("error sending http request: %w", err)
 	}
 	defer response.Body.Close()
-	// fmt.Printf("status code: %d\n", response.StatusCode)
+	//fmt.Printf("status code: %d\n", response.StatusCode)
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return "", fmt.Errorf("error reading http response: %w", err)
+		return 0, "", fmt.Errorf("error reading http response: %w", err)
 	}
-
 	// fmt.Printf("Body: %s\n", body)
-	return string(body), nil
+	return response.StatusCode, string(body), nil
 }
